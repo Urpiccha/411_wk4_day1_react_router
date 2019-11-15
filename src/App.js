d5899ae5fc9b16de3be758e520b9c27e9c32d9b9
Urpiccha <ur.picha.hartman@gmail.com>
2019-11-14 20:39:23 -0600

    Here we GO

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
-// Write imports for Router & BrowserRouter here //
+import React from "react";
+import Navigation from "./components/Navigation";
+import Router from "./Router";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <Navigation />
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Navigation />
+				<Router />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/Router.js
+++ src/Router.js
@@ -8,10 +8,22 @@ to the About component.
 Remember to export the component at the end
 */
 
-import React from 'react'
-import { Switch, Route } from 'react-router'
-// Write component imports here //
+import React from "react";
+import { Switch, Route } from "react-router";
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
 
-
+const Router = () => {
+	return (
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/about" component={About} />
+			<Route exact path="/car" component={Car} />
+		</Switch>
+	);
+};
 
 // Start Router function here //
+
+export default Router;
